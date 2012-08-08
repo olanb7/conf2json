c16c2abaaa6455319fbbc5334924939c2bd65c7f
Olan Byrne <olanb7@gmail.com>
2012-08-08 01:45:19 +0100

    formatting was all over the place there..

--- conf2json.js
+++ conf2json.js
@@ -1,10 +1,9 @@
 var fs = require('fs'),
-	et = require('elementtree'),
-	file = 'example.xml',
-	data = fs.readFileSync(file).toString(),
-	etree =  et.parse(data),
-	json = {};
-
+    et = require('elementtree'),
+    file = 'example.xml',
+    data = fs.readFileSync(file).toString(),
+    etree = et.parse(data),
+    json = {};
 
 /**
  * Show attributes with empty values in the output.
@@ -19,7 +18,7 @@ var showEmpty = true,
  *
  * @type {String}
  */
-	jsonIndentation = "  ";
+    jsonIndentation = "  ";
 
 
 /**
@@ -30,30 +29,30 @@ var showEmpty = true,
  */
 var conf2json = function(a, b) {
 
-	var name, text;
+  var name, text;
 
-	for (var i = 0; i < a._children.length; i++) {
+  for (var i = 0; i < a._children.length; i++) {
 
-		name = a._children[i].attrib.Name;
+    name = a._children[i].attrib.Name;
 
-		if (showEmpty || a._children[i].text) {
-	
-			// text seems to be '\r\n   ' style string when value is object
-			text = a._children[i].text.replace(/\s+/g," ") || {};
+    if (showEmpty || a._children[i].text) {
 
-			if (text === " ") {
-				text = {};
-			}
+      // text seems to be '\r\n   ' style string when value is object
+      text = a._children[i].text.replace(/\s+/g, " ") || {};
 
-			b[name] = text;
-		}
-		conf2json(a._children[i], b[name]);
-	}
-	return JSON.stringify(b, null, jsonIndentation);
+      if (text === " ") {
+        text = {};
+      }
+
+      b[name] = text;
+    }
+    conf2json(a._children[i], b[name]);
+  }
+  return JSON.stringify(b, null, jsonIndentation);
 };
 
 var xpath = './/longTagName[@Name="Startpoint"]',
-	root = etree.findall(xpath)[0],
-	opwv = conf2json(root, json);
+    root = etree.findall(xpath)[0],
+    opwv = conf2json(root, json);
 
 console.log(opwv);
